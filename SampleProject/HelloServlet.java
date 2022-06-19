f0326263e73ea2b96e1238fc3dc2c19a7fce63e3
ncsj <k-segawa@directec.co.jp>
2022-06-20 00:35:01 +0900

    HelloServlet.javaへテストコード（mainメソッド）を追加

--- SampleProject/HelloServlet.java
+++ SampleProject/HelloServlet.java
@@ -8,6 +8,10 @@ public class HelloServlet  extends HttpServlet{
         res.setContentType("text/html");
         PrintWriter out = res.getWriter();
 
+        print(out);
+    }
+
+    void print(PrintWriter out){
         out.println("<html>");
         out.println("<body>");
 
@@ -15,5 +19,13 @@ public class HelloServlet  extends HttpServlet{
 
         out.println("</body>");
         out.println("</html>");
+    }
+
+    public static void main(String args[]){
+        PrintWriter out = new PrintWriter(System.out);
+
+        HelloServlet hs = new HelloServlet();
+        hs.print(out);
+        out.flush();
     }
 }
